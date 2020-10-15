b35416e240210ddbbf42455f98a6af1195c9be67
Renato ST <renatostv@gmail.com>
2020-10-14 21:28:04 -0300

    fix error

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -5,7 +5,7 @@ import {FiArrowRight} from 'react-icons/fi'
 import '../styles/global.css';
 import '../styles/pages/landing.css'
 
-import LogoImg from '../Images/logo.svg'
+import LogoImg from '../images/logo.svg'
 import { Link } from 'react-router-dom';
 
 
