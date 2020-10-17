50348ce9fb0e0f0ec484e99da293f3faaa8f3366
Renato ST <renatostv@gmail.com>
2020-10-17 09:30:41 -0300

    finalização do projeto mobile com exibição dinâmica dos dados do backend e criação e gravação no banco de dados.

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from "react-native";
+import React, { useState, useCallback } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
-import {Feather} from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native';
-
+import { Feather } from "@expo/vector-icons";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 import mapMarker from "../images/map-marker.png";
+import api from "../servers/api";
 
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
 
 export default function OrphanagesMap() {
-    const navigation = useNavigation()
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+  const navigation = useNavigation();
 
-    function handleNavigateOrphanageDetail() {
-        navigation.navigate('OrphanageDetails')
-    }
+  useFocusEffect(
+    useCallback(() => {
+      api.get('orphanages').then(response => {
+        setOrphanages(response.data);
+      });
+    }, [])
+  );
 
-    return (
-        <View style={styles.container}>
+  function handleNavigateOrphanageDetail(id: number) {
+    navigation.navigate("OrphanageDetails", {id});
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate("SelectMapPosition");
+  }
+
+  return (
+    <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
@@ -26,92 +46,100 @@ export default function OrphanagesMap() {
           longitudeDelta: 0.01,
         }}
       >
-        <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x:2.5,
-            y:0.8,
-          }}
-          coordinate={{
-            latitude: -7.3647569,
-            longitude: -35.9008344,
-          }}
-        >
-          <Callout tooltip={true} onPress={handleNavigateOrphanageDetail}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das Meninas</Text>
-            </View>
-          </Callout>
-        </Marker>
+        {orphanages.map(orphanage => {
+          
+          return (
+            <Marker
+            key={orphanage.id}
+              icon={mapMarker}
+              calloutAnchor={{
+                x: 2.5,
+                y: 0.8,
+              }}
+              coordinate={{
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude,
+              }}
+            >
+              <Callout tooltip={true} onPress={() => handleNavigateOrphanageDetail(orphanage.id)}>
+                <View style={styles.calloutContainer}>
+                  <Text style={styles.calloutText}>{orphanage.name}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
       </MapView>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
 
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+        <RectButton
+          style={styles.createOrphanageButton}
+          onPress={handleNavigateToCreateOrphanage}
+        >
           <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  
-    map: {
-      width: Dimensions.get("window").width,
-      height: Dimensions.get("window").height,
-    },
-  
-    calloutContainer: {
-      width: 160,
-      height: 46,
-      paddingHorizontal: 16,
-      backgroundColor: 'rgba(255,255,255,0.8)',
-      borderRadius: 16,
-      justifyContent: 'center',
-    },
-  
-    calloutText: {
-      fontFamily: 'Nunito_700Bold',
-      color: '#0089a5',
-      fontSize: 12,
-    },
-  
-    footer:{
-      position: 'absolute',
-      left: 24,
-      right: 24,
-      bottom:32,
-  
-      backgroundColor: '#FFF',
-      borderRadius:20,
-      height: 56,
-      paddingLeft: 24,
-  
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-  
-      elevation: 3,
-    },
-  
-    footerText:{
-      fontFamily: 'Nunito_700Bold',
-      color: '#8fa7b3',
-    },
-  
-    createOrphanageButton:{
-      width: 56,
-      height: 56,
-      backgroundColor: '#15c3d6',
-      borderRadius: 20,
-  
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
-  
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255,255,255,0.8)",
+    borderRadius: 16,
+    justifyContent: "center",
+  },
+
+  calloutText: {
+    fontFamily: "Nunito_700Bold",
+    color: "#0089a5",
+    fontSize: 12,
+  },
+
+  footer: {
+    position: "absolute",
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    elevation: 3,
+  },
+
+  footerText: {
+    fontFamily: "Nunito_700Bold",
+    color: "#8fa7b3",
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import OrphanagesMap from './pages/OrphanagesMap';
-import OrphanageDetails from './pages/OrphanagesDetails';
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanagesDetails";
 
-const { Navigator, Screen} = createStackNavigator();
+import SelectMapPosition from "./pages/CreateOrphanages/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanages/OrphanageData";
+import Header from "./components/Header";
+
+const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
-            </Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: "#F2F3F5" },
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />,
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />,
+          }}
+        />
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />,
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
